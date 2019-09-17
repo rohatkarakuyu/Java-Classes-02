d430c0c96683fdc33029e8195d6c3c6564450fb1
19062738 <19062738@B105517.saultcollege.ca>
2019-09-17 10:30:37 -0400

    initilise

--- src/map103lab2/MAP103Lab2.java
+++ src/map103lab2/MAP103Lab2.java
@@ -1,11 +1,23 @@
-
 package map103lab2;
+
 import java.util.Scanner;
+
 public class MAP103Lab2 {
 
-
     public static void main(String[] args) {
-        // TODO code application logic here
+        try ( 
+                Scanner input = new Scanner(System.in)) {
+            int questionNumber = (int) (Math.random() * 5);
+            RandomQuizHelper helper = new RandomQuizHelper();
+            String question = helper.getQuestion(questionNumber);
+            String answer = "";
+            System.out.print(question);
+            answer = input.next();
+            if (helper.getResult(questionNumber, answer) == true) {
+                System.out.print("You are correct!");
+            } else {
+                System.out.print("You are not correct.");
+            }
+        }
     }
-    
 }
