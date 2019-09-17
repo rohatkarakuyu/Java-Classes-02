cd7bf548e8deccfdf45509706bd8dcd0d00cc9bf
19062738 <19062738@B105517.saultcollege.ca>
2019-09-17 10:30:48 -0400

    initilise

---
+++ src/map103lab2/RandomQuizHelper.java
@@ -0,0 +1,51 @@
+[09:58, 17/09/2019] Fırat: package map103lab2;
+
+public class RandomQuizHelper {
+    
+private String [] question = new String [5];
+
+RandomQuizHelper () {
+    question [0] = "What is the best model In the world ";
+    question [1] = "What is the best car In the world ";
+    question [2] = "What is the best city In the world ";
+    question [3] = "What is the best friend In the world ";
+    question [4] = "What is the best grocery store In the world ";
+
+    answer [0] = "Gigi Haddid";
+    answer [1] = "TOFAS Sahin";
+    answer [2] = "Ankara";
+    answer [3] = "Mr. Bean";
+    answer [4] = "Tesco";
+   
+}
+
+
+public String getQuestion(int indexFromPersonCallingThisMedhod){
+    return question (indexFromPersonCallingThisMedhod);
+}
+
+
+private String [] answer = new String [5];
+
+RandomQuizHelper () {    
+   
+    answer [0] = "";
+    answer [1] = "";
+    answer [2] = "";
+    answer [3] = "";
+    answer [4] = "";
+    
+
+private String getResult (int resultIndex){
+    
+    return"";
+}
+
+  public boolean getResult (int indexFromPersonCallingThisMedhod,String answers)
+          
+  if ("question" == "answer"){
+  return true;}
+   else {
+  return false ;
+    }
+ }
