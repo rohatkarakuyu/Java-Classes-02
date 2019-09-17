e6bed924e4e6fce72a29cb60e1363212390c486c
19062738 <19062738@B105517.saultcollege.ca>
2019-09-17 08:37:49 -0400

    initilise

---
+++ src/map103lab2/MAP103Lab2.java
@@ -0,0 +1,11 @@
+
+package map103lab2;
+import java.util.Scanner;
+public class MAP103Lab2 {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
