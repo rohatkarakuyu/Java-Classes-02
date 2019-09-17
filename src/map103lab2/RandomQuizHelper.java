33cba2dde286f134e6b35b452b8dda9339d4bc1d
19062738 <19062738@B105517.saultcollege.ca>
2019-09-17 11:03:34 -0400

    initilise

--- src/map103lab2/RandomQuizHelper.java
+++ src/map103lab2/RandomQuizHelper.java
@@ -1,51 +1,37 @@
-[09:58, 17/09/2019] Fırat: package map103lab2;
+package map103lab2;
 
 public class RandomQuizHelper {
-    
-private String [] question = new String [5];
 
-RandomQuizHelper () {
-    question [0] = "What is the best model In the world ";
-    question [1] = "What is the best car In the world ";
-    question [2] = "What is the best city In the world ";
-    question [3] = "What is the best friend In the world ";
-    question [4] = "What is the best grocery store In the world ";
+    private String[] questions = new String[5];
+    private String[] answers = new String[5];
 
-    answer [0] = "Gigi Haddid";
-    answer [1] = "TOFAS Sahin";
-    answer [2] = "Ankara";
-    answer [3] = "Mr. Bean";
-    answer [4] = "Tesco";
-   
-}
+    RandomQuizHelper() {
+        questions[0] = "What is the best model In the world ?";
+        questions[1] = "What is the best car In the world ?";
+        questions[2] = "What is the best city In the world  ?";
+        questions[3] = "What is the best friend In the world ?";
+        questions[4] = "What is the best grocery store In the world  ?";
 
-
-public String getQuestion(int indexFromPersonCallingThisMedhod){
-    return question (indexFromPersonCallingThisMedhod);
-}
-
-
-private String [] answer = new String [5];
-
-RandomQuizHelper () {    
-   
-    answer [0] = "";
-    answer [1] = "";
-    answer [2] = "";
-    answer [3] = "";
-    answer [4] = "";
-    
-
-private String getResult (int resultIndex){
-    
-    return"";
-}
-
-  public boolean getResult (int indexFromPersonCallingThisMedhod,String answers)
-          
-  if ("question" == "answer"){
-  return true;}
-   else {
-  return false ;
+        answers[0] = "Gigi Haddid";
+        answers[1] = "TOFAS Sahin";
+        answers[2] = "Ankara";
+        answers[3] = "Mr. Bean";
+        answers[4] = "Tesco";
     }
- }
+
+    public boolean getResult(int resultIndex, String answer) {
+
+        if ( answers[resultIndex].equals(answer) ){
+        return true;
+        } else {
+             return false;
+             }
+        
+    }
+
+    public String getQuestion(int getIndexResult) {
+
+        return questions[getIndexResult];
+    }
+
+}
